e0d2cb043555f96319234d82ecc34394961de612
Francesc Rosas <francescrosasbosque@gmail.com>
2019-10-11 11:53:45 +0100

    Refactor .eslintrc

--- project-files/.eslintrc.js
+++ project-files/.eslintrc.js
@@ -1,18 +1,15 @@
 /* eslint-env node */
 
+// See https://eslint.org/docs/user-guide/configuring
 module.exports = {
   extends: ["@frosas/eslint-config"],
   parserOptions: {
-    // sourceType: "module",
-    // ecmaVersion: 8,
-    // ecmaFeatures: {
-    //   experimentalObjectRestSpread: true,
-    //   jsx: true
-    // }
+    // For an ECMAScript version appropriate to a given Node version, check
+    // https://node.green
+    ecmaVersion: 2020
   },
   env: {
-    // es6: true,
-    // commonjs: true,
+    es6: true,
     // node: true,
     // browser: true
   }
