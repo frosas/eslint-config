4ff652847eb16360846d71fc179ce8583ddf81b6
Francesc Rosas <francesc@francescrosas.com>
2024-04-14 20:11:23 +0100

    Refactor

--- index.mjs
+++ index.mjs
@@ -3,6 +3,17 @@ import prettierPluginRecommended from "eslint-plugin-prettier/recommended"
 import tsEslint, { config } from "typescript-eslint"
 import reactConfigRecommended from "eslint-plugin-react/configs/recommended.js"
 
+const formattingConfig = config(prettierPluginRecommended, {
+  rules: { "prettier/prettier": ["error", { semi: false }] },
+})
+
+const tsConfig = config(...tsEslint.configs.recommended, {
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
+}).map((config) => ({ ...config, files: ["**/*.{ts,tsx}"] }))
+
 const reactConfig = config({
   files: ["**/*.{jsx,tsx}"],
   ...reactConfigRecommended,
@@ -14,17 +25,6 @@ const reactConfig = config({
     },
   },
 })
-
-const formattingConfig = config(prettierPluginRecommended, {
-  rules: { "prettier/prettier": ["error", { semi: false }] },
-})
-
-const tsConfig = config(...tsEslint.configs.recommended, {
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-  },
-}).map((config) => ({ ...config, files: ["**/*.{ts,tsx}"] }))
 
 export default config(
   js.configs.recommended,
