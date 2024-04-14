fefb53a499e91805c5303a3c58c7a4a52a8196b0
Francesc Rosas <francesc@francescrosas.com>
2024-04-14 15:17:39 +0100

    Refactor

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const js = require("@eslint/js")
 const prettierPluginRecommended = require("eslint-plugin-prettier/recommended")
 const { config } = require("typescript-eslint")
 
-module.exports = config(js.configs.recommended, prettierPluginRecommended, {
+const formattingConfig = config(prettierPluginRecommended, {
   rules: { "prettier/prettier": ["error", { semi: false }] },
 })
+
+module.exports = config(js.configs.recommended, ...formattingConfig)
