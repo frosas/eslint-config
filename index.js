6db9918d8d9d1e52b0b336699e41914ad6706a7b
Francesc Rosas <francesc@francescrosas.com>
2024-04-14 15:15:02 +0100

    Improve config types

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const js = require("@eslint/js")
 const prettierPluginRecommended = require("eslint-plugin-prettier/recommended")
+const { config } = require("typescript-eslint")
 
-/** @type import('eslint').Linter.FlatConfig[] */
-module.exports = [
-  js.configs.recommended,
-  prettierPluginRecommended,
-  { rules: { "prettier/prettier": ["error", { semi: false }] } },
-]
+module.exports = config(js.configs.recommended, prettierPluginRecommended, {
+  rules: { "prettier/prettier": ["error", { semi: false }] },
+})
