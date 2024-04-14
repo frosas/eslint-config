83383fb97829c61144be3bf072a5c0ea692c85c6
Francesc Rosas <francesc@francescrosas.com>
2024-04-14 15:23:35 +0100

    Lint TS

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const js = require("@eslint/js")
 const prettierPluginRecommended = require("eslint-plugin-prettier/recommended")
-const { config } = require("typescript-eslint")
+const { default: tsEslint, config } = require("typescript-eslint")
 
 const formattingConfig = config(prettierPluginRecommended, {
   rules: { "prettier/prettier": ["error", { semi: false }] },
 })
 
-module.exports = config(js.configs.recommended, ...formattingConfig)
+const tsConfig = config(...tsEslint.configs.recommended, {
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
+})
+
+module.exports = config(
+  js.configs.recommended,
+  ...formattingConfig,
+  ...tsConfig,
+)
