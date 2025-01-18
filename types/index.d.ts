94e0db34fbcd68f9dfef7c15d8f6c641e9be03e7
Francesc Rosas <francesc@francescrosas.com>
2025-01-18 13:13:54 +0000

    npm run generate-types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-declare const _default: import("typescript-eslint/node_modules/@typescript-eslint/utils/ts-eslint").FlatConfig.ConfigArray;
+declare const _default: import("@typescript-eslint/utils/ts-eslint").FlatConfig.ConfigArray;
 export default _default;
 import { config } from "typescript-eslint";
 import globals from "globals";
